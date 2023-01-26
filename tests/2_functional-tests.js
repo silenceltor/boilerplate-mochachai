7ac7e5aa3b1ad942dd064c9d596272829d042c87
Replit user <>
2023-01-26 22:12:55 +0000

    Most updated

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,49 +15,68 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
+    
     // #2
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
+    
     // #3
     test('Send {surname: "Colombo"}', function (done) {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({ "surname": 'Colombo'})
         .end(function (err, res) {
-          assert.fail();
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
 
           done();
         });
     });
+    
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({ "surname": "da Verrazzano" })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
+          
+          done();
+        });
     });
   });
 });
-
+/*
 const Browser = require('zombie');
+Browser.site = 'https://boilerplate-mochachai.silenceltor.repl.co';
 
 suite('Functional Tests with Zombie.js', function () {
+  const browser = new Browser();
   this.timeout(5000);
 
-
+  suiteSetup(function (done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
@@ -68,10 +87,18 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          
+          done();
+        });
+      });
     });
+    
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
       assert.fail();
@@ -80,3 +107,4 @@ suite('Functional Tests with Zombie.js', function () {
     });
   });
 });
+*/
